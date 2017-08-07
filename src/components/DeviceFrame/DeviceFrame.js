061be1e7421fa3d58c1a3c55ada8aaa9a4fde3a1
kimispencer <kimispencer@gmail.com>
2017-08-07 18:19:57 -0400

    adjust DeviceFrame scroll animation

--- src/components/DeviceFrame/DeviceFrame.js
+++ src/components/DeviceFrame/DeviceFrame.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './DeviceFrame.css';
 
+import DesktopScreenShot from '../../../public/assets/projects/onomie/00.png';
+import PhoneScreenShot from '../../../public/assets/projects/onomie/01.png';
+
 class DeviceFrame extends React.Component {
 	render() {
-		let desktop_screenshot = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/451895/datauri-generator-preview.jpg';
-		let phone_screenshot = 'http://www.planwallpaper.com/static/images/Awesome-iPhone-Wallpapers.jpg';
+		// let desktop_screenshot = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/451895/datauri-generator-preview.jpg';
+		// let phone_screenshot = 'http://www.planwallpaper.com/static/images/Awesome-iPhone-Wallpapers.jpg';
 		return (
 			<div className="device-frame">
 				{/* macbook laptop */}
 				<div className="macbook">
 					<div className="screen">
-						<div className="viewport" style={ {backgroundImage: 'url(' + desktop_screenshot + ')'} }></div>
+						<div className="viewport" style={ {backgroundImage: 'url(' + DesktopScreenShot + ')'} }></div>
 					</div>
 					<div className="base"></div>
 					<div className="notch"></div>
@@ -24,7 +27,7 @@ class DeviceFrame extends React.Component {
 							<span className="speaker"></span>
 						</div>
 						<div className="iphone-screen">
-							<div className="viewport" style={ {backgroundImage: 'url(' + phone_screenshot + ')'} }></div>
+							<div className="viewport" style={ {backgroundImage: 'url(' + PhoneScreenShot + ')'} }></div>
 						</div>
 						<div className="buttons">
 							<span className="on-off"></span>
