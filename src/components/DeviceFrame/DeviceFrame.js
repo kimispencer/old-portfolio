850542f2de3af8fd8a646b1018b60d45eb2a6dbf
kimispencer <kimispencer@gmail.com>
2017-08-07 14:49:53 -0400

    DeviceFrame, includes CSS iPhone frame + scrollable screenshot

--- src/components/DeviceFrame/DeviceFrame.js
+++ src/components/DeviceFrame/DeviceFrame.js
@@ -3,19 +3,40 @@ import './DeviceFrame.css';
 
 class DeviceFrame extends React.Component {
 	render() {
-		let screenshot = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/451895/datauri-generator-preview.jpg';
+		let desktop_screenshot = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/451895/datauri-generator-preview.jpg';
+		let phone_screenshot = 'http://www.planwallpaper.com/static/images/Awesome-iPhone-Wallpapers.jpg';
 		return (
 			<div className="device-frame">
 				{/* macbook laptop */}
 				<div className="macbook">
 					<div className="screen">
-						<div className="viewport" style={ {backgroundImage: 'url(' + screenshot + ')'} }></div>
+						<div className="viewport" style={ {backgroundImage: 'url(' + desktop_screenshot + ')'} }></div>
 					</div>
 					<div className="base"></div>
 					<div className="notch"></div>
 				</div>
 				{/* iphone */}
-				
+				<div className="iphone">
+					<div className="iphone-top">
+						<span className="camera"></span>
+						<span className="sensor"></span>
+						<span className="speaker"></span>
+					</div>
+					<div className="top-bar"></div>
+					<div className="iphone-screen">
+						<div className="viewport" style={ {backgroundImage: 'url(' + phone_screenshot + ')'} }></div>
+					</div>
+					<div className="buttons">
+						<span className="on-off"></span>
+						<span className="sleep"></span>
+						<span className="up"></span>
+						<span className="down"></span>
+					</div>
+					<div className="bottom-bar"></div>
+					<div className="iphone-bottom">
+						<span></span>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/views/Projects/ProjectDetail.js
+++ src/views/Projects/ProjectDetail.js
@@ -28,17 +28,14 @@ class ProjectDetail extends React.Component {
 				<h3 className="title">{project.name}</h3>
 				<ImageLoader className="project-detail-cover" src={project.coverImg} />
 
-				<section className="project-screenshots">
-					{/* project.projectScreenshots.map(img, index) => <DeviceFrame /> */}
-					<DeviceFrame />
-				</section>
-
 				<section className="text-container flex-row responsive">
 					<div className="text three-quarter-span" id="ProjectDescription">
 						<p className="title uppercase bold">project description</p>
 						<p>{project.intro}</p>
-						<br />
-						<p>{project.intro}</p>
+						<section className="project-screenshots">
+							{/* project.projectScreenshots.map(img, index) => <DeviceFrame /> */}
+							<DeviceFrame />
+						</section>
 						<ul className="project-image-list">
 						{project.projectImgs 
 							? project.projectImgs.map((img, index) => 
