f960235cb8e0ed8302e19650e229320a4d268799
kimispencer <kimispencer@gmail.com>
2017-08-16 20:42:26 -0400

    wahoo, just use pure THREE.js (instead of react-three-renderer)

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,53 +1,77 @@
 import React from 'react';
-import React3 from 'react-three-renderer';
 import * as THREE from 'three';
-// import ReactDOM from 'react-dom';
 import './Home.css';
 
-export default class Home extends React.Component {
-	constructor(props, context) {
-		super(props, context);
+class Home extends React.Component {
 
-		// construct the position vector here, because if we use 'new' within render, 
-		// React will think that things have changed when they have not. 
-		this.cameraPosition = new THREE.Vector3(0, 0, 5);
-
-		this.state = {
-			cubeRotation: new THREE.Euler(),
-		};
-
-		this._onAnimate = () => {
-			// we will get this callback every frame 
-
-			// pretend cubeRotation is immutable. 
-			// this helps with updates and pure rendering. 
-			// React will be sure that the rotation has now updated. 
-			this.setState({
-				cubeRotation: new THREE.Euler(
-					this.state.cubeRotation.x + 0.01,
-					this.state.cubeRotation.y + 0.01,
-					0
-				),
-			});
-		};
+	constructor(props) {
+		super(props);
 	}
 
-	render() {
-		const width = window.innerWidth; // canvas width 
-		const height = window.innerHeight; // canvas height 
+	componentDidMount() {
+		var height = document.getElementsByClassName('home')[0].clientHeight,
+			width = document.getElementsByClassName('home')[0].clientWidth;
 
+		// 3 must haves - SCENE , CAMERA, RENDERER
+
+		var scene = new THREE.Scene(); // Creates a new scene
+
+		var camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 ); // Creates a camera and passes (field of view, aspect ratio, near clipping plane, far clipping plane)
+			  camera.position.set(0, 0, 5);// moves the camera back some so we won't be inside of the cube
+			  camera.lookAt( scene.position ); // makes the camera always point toward the scene
+			  scene.add(camera);
+
+		var light = new THREE.PointLight( 0xFFFF00 );
+			  light.position.set( 10, 0, 10 );
+			  scene.add(light);
+
+		var renderer = new THREE.WebGLRenderer();
+			  renderer.setSize( width, height ); // sets size of render to the screen size
+			  document.getElementsByClassName('home')[0].appendChild( renderer.domElement); // Renders a canvas tag to the DOM
+
+		var geometry = new THREE.BoxGeometry( 2, 2, 2); // give the cube it's dimensions (width, height, depth)
+		var material = new THREE.MeshLambertMaterial( { color: 0xFF0000, wireframe: false} ); // creates material and gives it a color
+		material.wireframe = false;
+		var cube1 = new THREE.Mesh( geometry, material ); // crates the cube using the geometry and the material
+		var cube2 = new THREE.Mesh( geometry, material );
+			  cube2.position.set(5, -2, -5);
+		var cube3 = new THREE.Mesh( geometry, material );
+			  cube3.position.set(-5, -2, -5);
+
+		scene.add( cube1, cube2, cube3); // adds the cube to the scene
+
+		// Resize Three.js scene on window resize
+		window.addEventListener( 'resize', onWindowResize, false );
+
+		function onWindowResize(){
+
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+
+			renderer.setSize( width, height );
+
+		}
+
+		// Render loop to display cube
+		function render() {
+			requestAnimationFrame( render ); // requestAnimationFrame will pause when the user navigates to a new tab
+			cube1.rotation.z += 0.01;
+			cube1.rotation.x += 0.01;
+			cube1.rotation.y += 0.01;  // Runs every frame giving it the animation
+
+			cube2.rotation.x += 0.01; 
+
+			cube3.rotation.y += 0.01;
+
+			renderer.render( scene, camera ); 
+		};
+
+		render();
+	}
+	render() {
 		return (
-			<React3
-				mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below 
-				width={width} height={height} onAnimate={this._onAnimate} >
-				<scene>
-					<perspectiveCamera name="camera" fov={75} aspect={width / height} near={0.1} far={1000} position={this.cameraPosition} />
-					<mesh rotation={this.state.cubeRotation} >
-						<boxGeometry width={1} height={1} depth={1} />
-						<meshBasicMaterial color={0xffffff} />
-					</mesh>
-				</scene>
-			</React3>
+			<div className='home' id="Home"></div>
 		);
 	}
 }
+export default Home;
