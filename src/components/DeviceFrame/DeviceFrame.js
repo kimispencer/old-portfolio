76cf2e381e93ba294008fd1df40b99a13cdcce15
kimispencer <kimispencer@gmail.com>
2017-09-01 14:06:16 -0400

    DeviceFrame, scrollBounce

--- src/components/DeviceFrame/DeviceFrame.js
+++ src/components/DeviceFrame/DeviceFrame.js
@@ -1,47 +1,58 @@
 import React from 'react';
 import './DeviceFrame.css';
 
-const DeviceFrame = (props) => {
-	let img;
-	(props.src.indexOf('http') > -1) ? img = props.src : img = require(`../../../public/assets/${props.imgKey}/${props.src}`);
-	return (
-		<div className="device-frame">
-			{props.type === "desktop" && 
-			<div className="macbook">
-				<div className="screen">
-					<div className="viewport">
-						<img className="screenshot" src={img} role="presentation" />
-					</div>
-				</div>
-				<div className="base"></div>
-				<div className="notch"></div>
-			</div>
-			}
-			{props.type === "phone" && 
-			<div className="iphone">
-				<div className="content">
-					<div className="iphone-top">
-						<span className="camera"></span>
-						<span className="sensor"></span>
-						<span className="speaker"></span>
-					</div>
-					<div className="iphone-screen">
-						<img className="screenshot" src={img} role="presentation" />
-					</div>
-					<div className="buttons">
-						<span className="on-off"></span>
-						<span className="sleep"></span>
-						<span className="up"></span>
-						<span className="down"></span>
-					</div>
-					<div className="iphone-bottom">
-						<span></span>
-					</div>
-				</div>
-			</div>
+class DeviceFrame extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			bounce: false
 		}
-		</div>
-	);
+		this._scrollBounce = this._scrollBounce.bind(this);
+	}
+	_scrollBounce() {
+		this.setState({ bounce: !this.state.bounce });
+	}
+	render() {
+		let img = (this.props.src.indexOf('http') > -1) ? img = this.props.src : img = require(`../../../public/assets/${this.props.imgKey}/${this.props.src}`);
+		return (
+			<div className={`device-frame ${this.state.bounce ? 'bounce' : ''}`}>
+				<p onClick={this._scrollBounce}>click me</p>
+				{this.props.type === "desktop" && 
+				<div className="macbook">
+					<div className="screen">
+						<div className="viewport">
+							<img className="screenshot" src={img} role="presentation" />
+						</div>
+					</div>
+					<div className="base"></div>
+					<div className="notch"></div>
+				</div>
+				}
+				{this.props.type === "phone" && 
+				<div className="iphone">
+					<div className="content">
+						<div className="iphone-top">
+							<span className="camera"></span>
+							<span className="sensor"></span>
+							<span className="speaker"></span>
+						</div>
+						<div className="iphone-screen">
+							<img className="screenshot" src={img} role="presentation" />
+						</div>
+						<div className="buttons">
+							<span className="on-off"></span>
+							<span className="sleep"></span>
+							<span className="up"></span>
+							<span className="down"></span>
+						</div>
+						<div className="iphone-bottom">
+							<span></span>
+						</div>
+					</div>
+				</div>
+			}
+			</div>
+		);
+	}
 }
-
 export default DeviceFrame;
