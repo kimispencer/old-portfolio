a6126735d25a8daa6e2029db1ff052581acbf655
kimispencer <kimispencer@gmail.com>
2017-09-24 11:11:06 -0400

    Contact page

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import LinkedIn from '../../../public/assets/logos/linkedin.svg';
+import Github from '../../../public/assets/logos/github.svg';
 import './Contact.css';
 
 const Contact = () => (
     <div className="contact">
-        <div className="resume-container">
-            <div className="text center">
-                <p className="monospace">hello@kimispencer.com</p>
-                <p className="monospace">Brooklyn, NY</p>
-            </div>
-            <div id="ContactForm" className="flex-center flex-col">
-                <form>
-                    <div className="flex-col">
-                        <input id="Name" type="text" placeholder="Your Name" />
-                        <input id="Email" type="email" placeholder="Your Email" />
-                        <textarea id="Message" placeholder="Your Message"></textarea>
-                        <button type="submit">SUBMIT</button>
-                    </div>
-                </form>
-            </div>
+        <div className="text center">
+            <h3 className="title bold">Let's make cool things together!</h3>
+            <a href="mailto:kimispencer@gmail.com"><h4 className="monospace underline" id="Hello">hello@kimispencer.com</h4></a>
+            <h4 className="monospace">Brooklyn, NY</h4>
         </div>
+        {/*
+        <div id="ContactForm" className="flex-center flex-col">
+            <form>
+                <div className="flex-col">
+                    <input id="Name" type="text" placeholder="Your Name" />
+                    <input id="Email" type="email" placeholder="Your Email" />
+                    <textarea id="Message" placeholder="Your Message"></textarea>
+                    <button type="submit">SUBMIT</button>
+                </div>
+            </form>
+        </div>
+        */}
     </div>
 )
 export default Contact;
